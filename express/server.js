436542adf9c5b06841c109cc6ce26a0c664ad9f2
shadowdreamer <uzumaki@yeah.net>
2019-06-16 18:14:20 +0800

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,18 +1,20 @@
-const axios = require('axios');
+'use strict';
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-router.get('/', (req, res) => res.json({ route: req.originalUrl }));
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
-app.use(express.static('public'))
+
 app.use(bodyParser.json());
-app.use('/test', router)
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
-module.exports.handler = serverless(app);
-
 module.exports = app;
+module.exports.handler = serverless(app);
