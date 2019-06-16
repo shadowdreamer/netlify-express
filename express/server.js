bc376bc713ad2e8ba2796ab9368fe61404161ad2
shadowdreamer <uzumaki@yeah.net>
2019-06-16 19:36:50 +0800

    add schedule

--- express/server.js
+++ express/server.js
@@ -6,28 +6,39 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 const axios = require('axios');
+const schedule = require('node-schedule')
 
 const data = {
   version: '',
-  cards: ''
+  cards: '',
+  status:''
 }
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+function getData(ver) {
+  data.status = 'updating'
+  console.log('getting new card info')
+  axios('https://api.matsurihi.me/mltd/v1/cards?prettyPrint=false').then(resp => {
+    console.log('get new card info successed')
+    data.version = ver
+    data.cards = resp.data
+    data.status = 'updated'
+  })
+}
+function checkUpdate() {
   axios('https://api.matsurihi.me/mltd/v1/version/latest').then(e => {
     console.log('check card update')
-    if (e.data.res.updateTime == data.version) {
-      res.write(data.cards);
-      res.end();
-    } else {
-      data.version = e.data.res.updateTime
-      axios('https://api.matsurihi.me/mltd/v1/cards?prettyPrint=false').then(resp => {
-        console.log('get new card info')
-        data.cards = JSON.stringify(resp.data)
-        res.write(data.cards)
-        res.end()
-      })
+    if (e.data.res.updateTime != data.version) {
+      getData(e.data.res.updateTime)
     }
   })
+}
+getData()
+schedule.scheduleJob('30 39 3 * *', checkUpdate)
+
+router.get('/', (req, res) => {
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.json(data)
+  res.end()
 });
 
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
