1059f1e66a56003c533e557eba9a5acd6e7b27a1
shadowdreamer <uzumaki@yeah.net>
2019-06-16 18:19:19 +0800

    Update server.js

--- express/server.js
+++ express/server.js
@@ -5,11 +5,31 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
+const axios = require('axios');
+
+const data = {
+  version: '',
+  cards: ''
+}
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  axios('https://api.matsurihi.me/mltd/v1/version/latest').then(e => {
+    console.log('check card update')
+    if (e.data.res.updateTime == data.version) {
+      res.write(data.cards);
+      res.end();
+    } else {
+      data.version = e.data.res.updateTime
+      axios('https://api.matsurihi.me/mltd/v1/cards?prettyPrint=false').then(resp => {
+        console.log('get new card info')
+        data.cards = JSON.stringify(resp.data)
+        res.write(data.cards)
+        res.end()
+      })
+    }
+  })
 });
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
