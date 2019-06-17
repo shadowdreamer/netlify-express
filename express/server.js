f462a9f71ea15c9cd523cb3295f1123de74e26d3
shadowdreamer <uzumaki@yeah.net>
2019-06-17 10:16:30 +0800

    Update server.js

--- express/server.js
+++ express/server.js
@@ -7,37 +7,47 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 const axios = require('axios');
 
-const data = {
-  version: '',
+const localData = {
+  version: 'not ready',
   cards: '',
 }
-router.get('/', (req, res) => {
-  
-  axios('https://api.matsurihi.me/mltd/v1/version/latest').then(e => {
-    console.log('check card update')
-    if (e.data.res.updateTime != data.version) {
-      console.log('getting new card info')
-      axios('https://api.matsurihi.me/mltd/v1/cards?prettyPrint=false').then(resp => {
-        console.log('get new card info successed')
-        data.version = e.data.res.updateTime
-        data.cards = resp.data
-        res.json(data)
-        res.end()
-      })
-    } else {
-      console.log('res directly')
-      res.json(data)
-      res.end()
-    }
-  })
-
-
+function checkVersion(){
+  console.log('checking version')
+  return axios('https://api.matsurihi.me/mltd/v1/version/latest')
+}
+function getData(){
+  console.log('getting data')
+  return axios('https://api.matsurihi.me/mltd/v1/cards/33?prettyPrint=false')
+}
+async function checkAndGet(){
+  let serverVer = await checkVersion()
+  if( serverVer.data.res.updateTime != localData.version){
+    console.log('updating data')
+    let serverCards = await getData()
+    localData.version = serverVer.data.res.updateTime
+    localData.cards = serverCards.data
+    return 
+  }else{
+    console.log('need not update')
+    return
+  }
+}
+router.get('/', async (req, res) => {  
+  await checkAndGet()
+  return res.json(localData)   
 });
 
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/another', async (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', async (req, res) => {
+  console.log(req.body)
+  if(req.body.version == localData.version){
+    return res.json(localData)
+  }else{
+    await checkAndGet()
+    return res.json(localData)
+  }
+});
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-
 module.exports = app;
 module.exports.handler = serverless(app);
