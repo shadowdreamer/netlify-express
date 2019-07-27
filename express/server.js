155b6ac6bf73f4d4ecd40591d5248f6ba60cd371
shadowdreamer <uzumaki@yeah.net>
2019-07-27 10:03:28 +0800

    🐷

--- express/server.js
+++ express/server.js
@@ -1,52 +1,54 @@
-'use strict';
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-const bodyParser = require('body-parser');
+'use strict'
+const express = require('express')
+const serverless = require('serverless-http')
+const app = express()
+const bodyParser = require('body-parser')
 
-const router = express.Router();
-const axios = require('axios');
+const router = express.Router()
+const axios = require('axios')
 
-const localData = {
-  version: 'not ready',
-  cards: '',
-}
-function checkVersion(){
+let cards = []
+let localVer = 'not ready'
+function checkVersion () {
   console.log('checking version')
   return axios('https://api.matsurihi.me/mltd/v1/version/latest')
 }
-function getData(){
+function getData () {
   console.log('getting data')
   return axios('https://api.matsurihi.me/mltd/v1/cards?prettyPrint=false')
 }
-async function checkAndGet(){
+async function checkAndGet () {
   let serverVer = await checkVersion()
-  if( serverVer.data.res.updateTime != localData.version){
+  if (serverVer.data.res.updateTime != localVer) {
     console.log('updating data')
     let serverCards = await getData()
-    localData.version = serverVer.data.res.updateTime
-    localData.cards = serverCards.data
-    return 
-  }else{
+    localVer = serverVer.data.res.updateTime
+    cards = serverCards.data
+    return
+  } else {
     console.log('need not update')
     return
   }
 }
-router.get('/', async (req, res) => {  
+router.get('/', async (req, res) => {
   await checkAndGet()
-  return res.json(localData)   
-});
-
-router.get('/another', async (req, res) => res.json({ route: req.originalUrl }));
+  return res.json(cards)
+})
+router.get('/another', async (req, res) => res.json({ route: req.originalUrl }))
 router.post('/', async (req, res) => {
-  if(req.body.version == localData.version){
-    return res.json(localData)
-  }else{
-    await checkAndGet()
-    return res.json(localData)
+  let shortData = []
+  if (req.body.localLength) {
+    shortData = cards.slice(req.body.localLength)
   }
-});
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-module.exports = app;
-module.exports.handler = serverless(app);
+  if (req.body.version == localVer) {
+    return res.json(shortData.length > 0 ? shortData : cards)
+  } else {
+    await checkAndGet()
+    return res.json(shortData.length > 0 ? shortData : cards)
+  }
+})
+app.use('/pub', express.static('public'))
+app.use(bodyParser.json())
+app.use('/.netlify/functions/server', router)  // path must route to lambda
+module.exports = app
+module.exports.handler = serverless(app)
